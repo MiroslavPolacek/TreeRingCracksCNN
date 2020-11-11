b6d3dccf0aeda03bc97797551d2d42f36fb81303
MiroslavPolacek <f.subbuteo@gmail.com>
2020-11-11 13:52:54 +0100

    Added bash script for RTX and specified all fields

--- Mask_RCNN/samples/TreeRing/TrainCracksRTX.sh
+++ Mask_RCNN/samples/TreeRing/TrainCracksRTX.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:V100:1
+#SBATCH --gres=gpu:RTX:1
 #SBATCH --partition=g
 #SBATCH --mem=112G
 #SBATCH --qos=medium
 #SBATCH --time=2-00:00:00
-#SBATCH --output=TrainCracks.stdout
+#SBATCH --output=TrainCracksRTX.stdout
 
 ml load anaconda3/2019.03
 source activate ~/.conda/envs/TreeRingCNN
