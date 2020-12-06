dce9c6075acdef9dc03008939c0e8c27e92d0212
MiroslavPolacek <f.subbuteo@gmail.com>
2020-12-06 11:33:37 +0100

    changed location of dataset because of memory problem in home dir

--- Mask_RCNN/samples/TreeRing/TrainCracksComb.sh
+++ Mask_RCNN/samples/TreeRing/TrainCracksComb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:V100:1
+#SBATCH --gres=gpu:RTX:1
 #SBATCH --partition=g
 #SBATCH --mem=112G
 #SBATCH --qos=long
@@ -11,4 +11,4 @@
 ml load anaconda3/2019.03
 source activate ~/.conda/envs/TreeRingCNN
 
-time ~/.conda/envs/TreeRingCNN/bin/python3 /users/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/TreeRingComb.py train --dataset=/users/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/datasets/treeringCombined  --weights=last
+time ~/.conda/envs/TreeRingCNN/bin/python3 /users/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/TreeRingComb.py train --dataset=/groups/swarts/user/miroslav.polacek/CNN/treeringCombined  --weights=last
