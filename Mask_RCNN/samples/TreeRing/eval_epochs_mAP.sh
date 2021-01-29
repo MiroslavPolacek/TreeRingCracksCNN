8085cf33ad4fb0898fe151ade0f86a91056d8bf2
Polacek <miroslav.polacek@clip-login-0.cbe.vbc.ac.at>
2021-01-29 15:42:00 +0100

    adjusting the runs

--- Mask_RCNN/samples/TreeRing/eval_epochs_mAP.sh
+++ Mask_RCNN/samples/TreeRing/eval_epochs_mAP.sh
@@ -3,12 +3,12 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:RTX:1
 #SBATCH --partition=g
-#SBATCH --mem=112G
-#SBATCH --qos=medium
-#SBATCH --time=2-00:00:00
-#SBATCH --output=eval_epochs_mAP_new_val.stdout
+#SBATCH --mem=32G
+#SBATCH --qos=short
+#SBATCH --time=0-08:00:00
+#SBATCH --output=eval_epochs_mAP_cracks.stdout
 
 ml load anaconda3/2019.03
 source activate ~/.conda/envs/TreeRingCNN
 
-time ~/.conda/envs/TreeRingCNN/bin/python3 /groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/eval_epochs_mAP_new.py --dataset=/groups/swarts/user/miroslav.polacek/CNN/val_new  --weight_folder=/users/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/logs/treeringcrackscomb20201119T2220/
+time ~/.conda/envs/TreeRingCNN/bin/python3 /groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/eval_epochs_mAP_new.py --dataset=/groups/swarts/user/miroslav.polacek/CNN/val_new  --weight_folder=/groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/logs/treeringcrackscomb2_onlycracks20210121T2224
