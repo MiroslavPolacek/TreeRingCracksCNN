021afaed820355a23962b926f9e8ee7bccb4f454
MiroslavPolacek <f.subbuteo@gmail.com>
2021-02-05 14:18:34 +0100

    Adding mAP for training on smaller dataset and only rings

---
+++ Mask_RCNN/samples/TreeRing/eval_epochs_mAP_ring_old.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:RTX:1
+#SBATCH --partition=g
+#SBATCH --mem=32G
+#SBATCH --qos=medium
+#SBATCH --time=2-00:00:00
+#SBATCH --output=eval_epochs_mAP_ring.stdout
+
+ml load anaconda3/2019.03
+source activate ~/.conda/envs/TreeRingCNN
+
+time ~/.conda/envs/TreeRingCNN/bin/python3 /groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/eval_epochs_mAP_new_ring.py --dataset=/groups/swarts/user/miroslav.polacek/CNN/val_new  --weight_folder=/groups/swarts/user/miroslav.polacek/CNN/Mask_RCNN/logs/traintestmlw220200727T1332
