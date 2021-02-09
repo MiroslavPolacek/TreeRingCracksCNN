cd2ad15da214a5f4b9bb39f704fdc3a321836fcd
MirolavPolaek <f.subbuteo@gmail.com>
2021-02-09 17:03:10 +0100

    Corrected a typo in a path

--- Mask_RCNN/samples/TreeRing/eval_weight_onlyCracks.sh
+++ Mask_RCNN/samples/TreeRing/eval_weight_onlyCracks.sh
@@ -11,4 +11,4 @@
 ml load anaconda3/2019.03
 source activate ~/.conda/envs/TreeRingCNN
 
-time ~/.conda/envs/TreeRingCNN/bin/python3 /groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/evaluate_weights_onlyCracks.py --dataset=/groups/swarts/user/miroslav.polacek/CNN/val_new  --weight=/users/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/logs/treeringcrackscomb2_onlycracks20210121T2224/mask_rcnn_treeringcrackscomb2_onlycracks_0512.h5
+time ~/.conda/envs/TreeRingCNN/bin/python3 /groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/evaluate_weights_onlyCracks.py --dataset=/groups/swarts/user/miroslav.polacek/CNN/val_new  --weight=/groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/logs/treeringcrackscomb2_onlycracks20210121T2224/mask_rcnn_treeringcrackscomb2_onlycracks_0512.h5
