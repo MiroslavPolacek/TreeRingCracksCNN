c7b41232ef484103efe0b09ed2cfc1cf8ce77fc3
MirolavPolaek <f.subbuteo@gmail.com>
2021-02-25 15:32:12 +0100

    Setup for training with mask loss weight 3

---
+++ Mask_RCNN/samples/TreeRing/TrainCracksComb3.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:RTX:1
+#SBATCH --partition=g
+#SBATCH --mem=64G
+#SBATCH --qos=long
+#SBATCH --time=14-00:00:00
+#SBATCH --output=TrainCracksComb3.stdout
+
+ml load anaconda3/2019.03
+source activate ~/.conda/envs/TreeRingCNN
+
+time ~/.conda/envs/TreeRingCNN/bin/python3 /groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/TreeRingComb3.py train --dataset=/groups/swarts/user/miroslav.polacek/CNN/treeringCombined  --weights=/groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/logs/treeringcrackscomb220210114T1540/mask_rcnn_treeringcrackscomb2_0228.h5
