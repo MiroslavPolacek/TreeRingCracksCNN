7e71ca135d2c52bbfcf6519d6b545c0ddc8b1481
Polacek <miroslav.polacek@clip-login-1.cbe.vbc.ac.at>
2021-02-25 16:43:28 +0100

    small corrections to run

--- Mask_RCNN/samples/TreeRing/TrainCracksComb3_onlyRing.sh
+++ Mask_RCNN/samples/TreeRing/TrainCracksComb3_onlyRing.sh
@@ -11,4 +11,4 @@
 ml load anaconda3/2019.03
 source activate ~/.conda/envs/TreeRingCNN
 
-time ~/.conda/envs/TreeRingCNN/bin/python3 /groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/TreeRingComb3_onlyRing.py train --dataset=/groups/swarts/user/miroslav.polacek/CNN/treeringCombined  --weights=last
+time ~/.conda/envs/TreeRingCNN/bin/python3 /groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/TreeRingComb3_onlyRing.py train --dataset=/groups/swarts/user/miroslav.polacek/CNN/treeringCombined  --weights=imagenet
