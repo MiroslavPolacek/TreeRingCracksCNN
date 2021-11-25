1646de706ac9886b668c208235c25f493279ad08
MirolavPolaek <f.subbuteo@gmail.com>
2021-11-25 15:11:59 +0100

    debug

--- Mask_RCNN/samples/TreeRing/eval_weight_realPost_cluster.sh
+++ Mask_RCNN/samples/TreeRing/eval_weight_realPost_cluster.sh
@@ -8,16 +8,12 @@
 #SBATCH --time=0-01:00:00
 #SBATCH --output=eval_real_post.stdout
 
-source activate TreeRingCNN
+ml load anaconda3/2019.03
+source activate ~/.conda/envs/TreeRingCNN
 
-FILES=(TreeRingComb2_onlyRing_0)
-
-for FILE in ${FILES[@]}
-do
-  time python3 \
-    /users/miroslav.polacek/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/evaluate_weights_realPost_simple.py \
-    --TreeRingConf=$FILE \
-    --dataset=/users/miroslav.polacek/eval_testing/val_new_subset \
-    --weight=/groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/logs/treeringcrackscomb2_onlyring20210121T1457/mask_rcnn_treeringcrackscomb2_onlyring_0186.h5 \
-    --path_out=/users/miroslav.polacek/eval_testing/results
-done
+time ~/.conda/envs/TreeRingCNN/bin/python3 \
+  /users/miroslav.polacek/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/evaluate_weights_realPost_simple.py \
+  --TreeRingConf=TreeRingComb2_onlyRing_0 \
+  --dataset=/users/miroslav.polacek/eval_testing/val_new_subset \
+  --weight=/groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/logs/treeringcrackscomb2_onlyring20210121T1457/mask_rcnn_treeringcrackscomb2_onlyring_0186.h5 \
+  --path_out=/users/miroslav.polacek/eval_testing/results
