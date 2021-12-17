42613f039effdf341ef85b7d5d3e2275fceacf03
MirolavPolaek <f.subbuteo@gmail.com>
2021-12-17 13:04:59 +0100

    rerun eval with smaller score steps for 3 days

--- Mask_RCNN/samples/TreeRing/eval_weight_realPostCracks_cluster.sh
+++ Mask_RCNN/samples/TreeRing/eval_weight_realPostCracks_cluster.sh
@@ -5,8 +5,8 @@
 #SBATCH --partition=g
 #SBATCH --mem=24G
 #SBATCH --qos=long
-#SBATCH --time=3-00:00:00
-#SBATCH --output=eval_real_post.stdout
+#SBATCH --time=2-12:00:00
+#SBATCH --output=eval_real_postCracks.stdout
 
 ml load anaconda3/2019.03
 source activate ~/.conda/envs/TreeRingCNN
@@ -15,5 +15,5 @@ time ~/.conda/envs/TreeRingCNN/bin/python3 \
   /users/miroslav.polacek/TreeRingCracksCNN/Mask_RCNN/samples/TreeRing/evaluate_weights_realPost_simple.py \
   --TreeRingConf=TreeRingComb2_onlyRing_0 \
   --dataset=/groups/swarts/user/miroslav.polacek/CNN/val_new \
-  --weight=/groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/logs/treeringcrackscomb2_onlyring20210121T1457/mask_rcnn_treeringcrackscomb2_onlyring_0186.h5 \
+  --weight=/groups/swarts/user/miroslav.polacek/github/TreeRingCracksCNN/Mask_RCNN/logs/treeringcrackscomb2_onlycracks20210121T2224/mask_rcnn_treeringcrackscomb2_onlycracks_0522.h5 \
   --path_out=/users/miroslav.polacek/eval_testing/results
